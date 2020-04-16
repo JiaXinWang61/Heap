f94dbc8f4935aeab3117b48ab011a126ab76c3a3
JiaXinWang61 <584937529@qq.com>
2020-04-16 12:27:26 +0800

    增加add 和 extractMax 方法

--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -40,4 +40,58 @@ public class MaxHeap<E extends Comparable<E>> {
         return index * 2 + 2;
     }
 
+    //向堆中添加元素
+    public void add(E e) {
+        data.add(e);
+        siftUp(data.size() - 1);
+    }
+
+    //上浮
+    private void siftUp(int k) {
+        while (k > 0 && data.get(parent(k)).compareTo(data.get(k)) < 0) {
+            swap(k, parent(k));
+            k = parent(k);
+        }
+    }
+
+    //看堆中最大元素
+    public E findMax() {
+        if (data.size() == 0) {
+            throw new IllegalArgumentException("Can not findMax when heap is empty.");
+        }
+        return data.get(0);
+    }
+
+    //取出堆中最大元素
+    public E extractMax() {
+        E ret = findMax();
+        swap(0,data.size()-1);
+        data.remove(data.size() - 1);
+        shiftDown(0);
+        return ret;
+    }
+
+    //下浮
+    private void shiftDown(int k) {
+        while (leftChild(k) < data.size()) {
+            int j = leftChild(k);
+            if (j + 1 < data.size() && data.get(j + 1).compareTo(data.get(j)) > 0) {
+                j = rightChild(k);
+            }
+            if (data.get(k).compareTo(data.get(j)) >= 0) {
+                break;
+            }
+            swap(k, j);
+            k = j;
+        }
+
+    }
+
+
+    private void swap(int k, int j) {
+        E temp = data.get(k);
+        data.set(k, data.get(j));
+        data.set(j, temp);
+    }
+
 }
